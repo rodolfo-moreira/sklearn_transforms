bbcc304428885c9f377dcd1e9e42d0baf6f94b4e
rodolfo-moreira <rodolformoreira@gmail.com>
2020-08-21 19:53:07 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor, XGBClassifier
+
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +17,38 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class XGBoostWithEarlyStop(BaseEstimator):
+    def __init__(self, early_stopping_rounds=5, test_size=0.1, 
+                 eval_metric='mae', **estimator_params):
+        self.early_stopping_rounds = early_stopping_rounds
+        self.test_size = test_size
+        self.eval_metric=eval_metric='mae'        
+        if self.estimator is not None:
+            self.set_params(**estimator_params)
+
+    def set_params(self, **params):
+        return self.estimator.set_params(**params)
+
+    def get_params(self, **params):
+        return self.estimator.get_params()
+
+    def fit(self, X, y):
+        x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=self.test_size)
+        self.estimator.fit(x_train, y_train, 
+                           early_stopping_rounds=self.early_stopping_rounds, 
+                           eval_metric=self.eval_metric, eval_set=[(x_val, y_val)])
+        return self
+
+    def predict(self, X):
+        return self.estimator.predict(X)
+
+class XGBoostRegressorWithEarlyStop(XGBoostWithEarlyStop):
+    def __init__(self, *args, **kwargs):
+        self.estimator = XGBRegressor()
+        super(XGBoostRegressorWithEarlyStop, self).__init__(*args, **kwargs)
+
+class XGBoostClassifierWithEarlyStop(XGBoostWithEarlyStop):
+    def __init__(self, *args, **kwargs):
+        self.estimator = XGBClassifier()
+        super(XGBoostClassifierWithEarlyStop, self).__init__(*args, **kwargs)
